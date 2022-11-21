11a8cebc345bc40cbc23be77a050f68c4c6c0c23
qqrm <qqrm@mail.ru>
2022-11-21 07:18:25 +0300

    improve benchs

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,17 @@
 extern crate test;
 
 use rayon::prelude::*;
-const MAX_ITER: usize = 8;
 
-fn split_computation<F, T>(min_split_size: usize, data: Vec<T>, f: F) -> Vec<T>
+fn split_computation<F, T>(split_size: usize, max_iter: usize, data: Vec<T>, f: F) -> Vec<T>
 where
-    F: Fn(T) -> T,
+    F: Fn(T, usize) -> T,
     F: Sync + Send,
     T: Copy + Sync + Send,
 {
-    if min_split_size > data.len() {
-        return data.iter().map(|el| f(*el)).collect();
+    if split_size < data.len() {
+        return data.par_iter().map(|el| f(*el, max_iter)).collect();
     } else {
-        return data.par_iter().map(|el| f(*el)).collect();
+        return data.iter().map(|el| f(*el, max_iter)).collect();
     }
 }
 
@@ -23,8 +22,8 @@ fn is_even(n: u64) -> bool {
 }
 
 /// Algorithm from task
-fn calc_num(mut num: u64) -> u64 {
-    for i in 0..MAX_ITER {
+fn calc_num(mut num: u64, max_iter: usize) -> u64 {
+    for i in 0..max_iter {
         if 1 == num {
             return i as u64;
         }
@@ -47,17 +46,16 @@ mod tests {
         let data = vec![1, 2, 3, 100];
         let expected_res = vec![0, 1, 7, 88];
 
-        let res = split_computation(10000usize, data, calc_num);
+        let res = split_computation(10000usize, 8usize, data, calc_num);
 
         assert_eq!(res, expected_res);
     }
 
     #[test]
     fn test_many_numbers() {
-        const MIN_SPLIT_SIZE: usize = 2;
         let data = (1u64..1_000_001).collect();
 
-        let res = split_computation(2000000usize, data, calc_num);
+        let res = split_computation(1_000_002usize, 19usize, data, calc_num);
 
         assert_eq!(res.len(), 1000000usize);
     }
@@ -67,7 +65,7 @@ mod tests {
         const MIN_SPLIT_SIZE: usize = 2;
         let data = (1u64..1_000_001).collect();
 
-        let res = split_computation(10000usize, data, calc_num);
+        let res = split_computation(500_000usize, 19usize, data, calc_num);
 
         assert_eq!(res.len(), 1000000usize);
     }
